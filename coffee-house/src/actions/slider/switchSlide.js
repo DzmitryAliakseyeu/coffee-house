be1a58684d2c4398804acd83e26388e2bae767b2
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-10-02 12:29:49 +0200

    feat: add movement slide by click on the button inside favorite section for index.html desktop version (Thu, Okt 02, 2025 12:29:09 PM)

---
+++ coffee-house/src/actions/slider/switchSlide.js
@@ -0,0 +1,7 @@
+export default function switchSlide(i){
+    const rowSlider = document.querySelector('.row-slider');
+    if(rowSlider){
+        rowSlider.style.transform = `translateX(-${i * 100}%)`;
+    }
+
+}
--- coffee-house/src/main/sections/favorite/slider/slider/slider.js
+++ coffee-house/src/main/sections/favorite/slider/slider/slider.js
@@ -1,16 +1,38 @@
+import switchSlide from "../../../../../actions/slider/switchSlide";
 import createButton from "../../../../../button/button";
 import createRowSlider from "./row-slider/row-slider";
 import './slider.css'
 import createSliderTrack from "./sliderTrack/sliderTrack";
 
 export default function createSlider(parent){
+
+    let index = 0; 
     const slider = document.createElement('div');
     slider.classList.add('slider');
     parent.append(slider);
 
-    createButton(slider, 'slider-left', ()=> console.log('left'), '', true);
-    createSliderTrack(slider)
-    // createRowSlider(slider)
-    createButton(slider, 'slider-right', ()=> console.log('right'), '', true);
+    createButton(slider, 'slider-left', ()=> {
+        let step = index - 1;
+        if(step < 0){
+            index = 2;
+            switchSlide(index);
+            return 
+        } 
+        switchSlide(step)
+    index = step;},
+         '', true);
+    createSliderTrack(slider);
+    createButton(slider, 'slider-right', ()=>{
+        let step = index + 1;
+        if(step > 2){
+            index = 0;
+            switchSlide(index)
+            return 
+        } 
+        switchSlide(step)
+        index = step
+
+    },
+         '', true);
 
 }
