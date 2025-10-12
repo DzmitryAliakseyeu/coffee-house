773a1d11a53f7c7797e6f54f26c4400d863995c2
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-10-12 19:32:49 +0200

    Updates

--- assets/main-C9b6S_BA.js
+++
@@ -1,1 +0,0 @@
-import{c as o,a as t,b as c}from"./main-Uec-a8oC.js";function d(){const a=document.body;console.log(a);const e=document.createElement("div");e.classList.add("app"),e.id="app",a.append(e),o(e),t(e),c(e)}d();
