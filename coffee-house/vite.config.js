09122219154609917ec82bc0d09cbb06d0d3315d
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-10-12 18:23:10 +0200

    chore: update vite config

--- coffee-house/vite.config.js
+++ coffee-house/vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  root: '.',  // project root
-  base: '/coffee-house/', // <-- important for GitHub Pages repo
+  root: '.', // project root
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public',   // source folder
+          dest: '',         // copy as-is into dist/public
+        },
+      ],
+    }),
+  ],
   build: {
-    outDir: 'dist', // or 'docs' if you host from docs folder
+    outDir: 'dist', // output folder
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
