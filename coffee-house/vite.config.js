bd842a618c41244cf584bcd9a6a87cbf78a54ec8
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-10-12 17:31:49 +0200

    refactor: add favicon (Sun, Okt 12, 2025, 17:31:28 PM)

--- coffee-house/vite.config.js
+++ coffee-house/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: '.',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, '/pages/menu.html'),
+        menu: resolve(__dirname, 'pages/menu.html'),
       },
     },
   },
