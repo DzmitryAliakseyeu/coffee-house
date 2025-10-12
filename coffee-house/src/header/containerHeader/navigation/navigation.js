3b4493a01a0bc5565567aa2a055460cf12a9de12
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-10-12 15:07:45 +0200

    feat: implement hidden burger menu by click on anchor (Sun, Okt 12, 2025, 15:07:22 PM)

--- coffee-house/src/header/containerHeader/navigation/navigation.js
+++ coffee-house/src/header/containerHeader/navigation/navigation.js
@@ -24,6 +24,19 @@ export default function createNavigation(parent) {
     link.classList.add('link-and-button');
     link.classList.add('text-dark');
 
+    link.addEventListener('click', ()=> {
+      const navigationBox= document.querySelector('.navigation-box');
+
+      if(navigationBox.classList.contains('open')){
+        navigationBox.classList.remove('open');
+        document.documentElement.classList.remove('no-scroll');
+        const buttonBurger = document.querySelector('.button-burger');
+         const buttonBurgerText = document.querySelector('.button-burger-text');
+        buttonBurger.classList.remove('open');
+         buttonBurgerText.classList.remove('open');
+      }
+    })
+
     link.href = item.href;
     link.text = item.text;
 
