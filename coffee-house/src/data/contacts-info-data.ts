aebe8e19531209257e12a67ce9a0b6acff2c2ab4
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-11-03 15:18:03 +0100

    test husky

--- coffee-house/src/data/contacts-info-data.ts
+++ coffee-house/src/data/contacts-info-data.ts
@@ -4,7 +4,7 @@ export const contactsInfoData: ContactsInfoDataI[] = [
   {
     srcImg: './contacts-info/pin-alt.svg',
 
-    href: 'https://maps.app.goo.gl/HcGzJkaL18asCgkq7',
+        href: 'https://maps.app.goo.gl/HcGzJkaL18asCgkq7',
     title: 'pin-alt',
     content: `8558 Green Rd.,   LA`,
     isLink: true,
